9dd1265ba0a6608c796474418d8946cdc2d57b9e
MattKrayson <150620712+MattKrayson@users.noreply.github.com>
2024-06-28 00:35:30 +0100

    Update sysinfo.sh

--- Useful Scripts/Linux/sysinfo.sh
+++ Useful Scripts/Linux/sysinfo.sh
@@ -7,11 +7,11 @@ cpu_frequencies=$(grep MHz /proc/cpuinfo | awk '{print $4}')
 cpu_temperatures=$(sensors | grep 'Core' | awk '{print $3}')
 
 # Get network activity (adjust 'eth0' to your network interface)
-network_activity=$(ifstat -i enp3s0 0.1 1 | awk 'NR==3 {print $1, $2}')
+#network_activity=$(ifstat -i enp3s0 0.1 1 | awk 'NR==3 {print $1, $2}')
 
 # Print headers for readability
-echo -e "CPU MHz   |   Temp   |   RX |  TX"
-echo -e "----------|----------|-------------"
+echo -e "CPU MHz   |   Temp   |
+echo -e "----------|----------|
 
 # Combine the output and format as a table
-paste <(echo "$cpu_frequencies") <(echo "$cpu_temperatures") <(echo "$network_activity") | awk -F'\t' '{printf "%-8s  | %-7s  | %-5s  %-5s \n"  , $1, $2, $3, $4}'
+paste <(echo "$cpu_frequencies") <(echo "$cpu_temperatures") <(echo "$network_activity") #| awk -F'\t' '{printf "%-8s  | %-7s  | %-5s  %-5s \n"  , $1, $2, $3, $4}'
