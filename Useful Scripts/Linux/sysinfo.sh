74d2ed6af4a8b5162dd14648c78d304f2fa28f8f
MattKrayson <150620712+MattKrayson@users.noreply.github.com>
2024-06-28 00:39:45 +0100

    Update sysinfo.sh

--- Useful Scripts/Linux/sysinfo.sh
+++ Useful Scripts/Linux/sysinfo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This script fetches CPU frequencies and temperatures
 
 # Get CPU frequencies
 cpu_frequencies=$(grep MHz /proc/cpuinfo | awk '{print $4}')
@@ -7,9 +6,15 @@ cpu_frequencies=$(grep MHz /proc/cpuinfo | awk '{print $4}')
 # Get CPU temperatures
 cpu_temperatures=$(sensors | grep 'Core' | awk '{print $3}')
 
+# Get network activity (adjust 'eth0' to your network interface)
+network_activity=$(ifstat -i enp3s0 0.1 1 | awk 'NR==3 {print $1, $2}')
+
 # Print headers for readability
-echo "CPU MHz   |   Temp   |"
-echo "----------|----------|"
+echo -e "CPU MHz   |   Temp   |   RX |  TX"
+echo -e "----------|----------|-------------"
 
 # Combine the output and format as a table
-paste <(echo "$cpu_frequencies") <(echo "$cpu_temperatures") | column -t
+paste <(echo "$cpu_frequencies") <(echo "$cpu_temperatures") <(echo "$network_activity") | awk -F'\t' '{printf "%-8s  | %-7s  | %-5s  %-5s \n"  , $1, $2, $3, $4}'
+
+# Run NVIDIA command
+nvidia-smi
