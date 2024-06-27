5c5a12ab0c937fd024d8916206d09b50e27914f4
MattKrayson <150620712+MattKrayson@users.noreply.github.com>
2024-06-28 00:38:30 +0100

    Update sysinfo.sh

--- Useful Scripts/Linux/sysinfo.sh
+++ Useful Scripts/Linux/sysinfo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#This requires lm-sensors andifstat to be installed
+# This script requires lm-sensors and ifstat to be installed
+
 # Get CPU frequencies
 cpu_frequencies=$(grep MHz /proc/cpuinfo | awk '{print $4}')
 
 # Get CPU temperatures
 cpu_temperatures=$(sensors | grep 'Core' | awk '{print $3}')
 
-
 # Print headers for readability
-echo -e "CPU MHz   |   Temp   |
-echo -e "----------|----------|
+echo -e "CPU MHz   |   Temp   |"
+echo -e "----------|----------|"
 
 # Combine the output and format as a table
 paste <(echo "$cpu_frequencies") <(echo "$cpu_temperatures")
